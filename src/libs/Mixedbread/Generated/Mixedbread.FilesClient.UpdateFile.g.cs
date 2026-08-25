e2b5c4913ec10ab669a3cf773f5e8bdb43efc371
Konstantin S. <havendv@gmail.com>
2026-08-25 05:21:59 +0400

    feat: Updated OpenAPI spec (#60)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Mixedbread/Generated/Mixedbread.FilesClient.UpdateFile.g.cs
+++ src/libs/Mixedbread/Generated/Mixedbread.FilesClient.UpdateFile.g.cs
@@ -190,10 +190,6 @@ namespace Mixedbread
             }
 
                             var __httpRequestContent = new global::System.Net.Http.MultipartFormDataContent();
-                            __httpRequestContent.Add(
-                                content: new global::System.Net.Http.StringContent(fileId.ToString() ?? string.Empty),
-                                name: "\"file_id\"");
-
                             var __contentFile = new global::System.Net.Http.ByteArrayContent(request.File ?? global::System.Array.Empty<byte>());
                             __contentFile.Headers.ContentType = new global::System.Net.Http.Headers.MediaTypeHeaderValue(
                                 request.Filename is null
@@ -700,10 +696,6 @@ namespace Mixedbread
             }
 
                             var __httpRequestContent = new global::System.Net.Http.MultipartFormDataContent();
-                            __httpRequestContent.Add(
-                                content: new global::System.Net.Http.StringContent(fileId.ToString() ?? string.Empty),
-                                name: "\"file_id\"");
-
                             var __contentFile = new global::System.Net.Http.StreamContent(file);
                             __contentFile.Headers.ContentType = new global::System.Net.Http.Headers.MediaTypeHeaderValue(
                                 request.Filename is null
@@ -1161,10 +1153,6 @@ namespace Mixedbread
             }
 
                             var __httpRequestContent = new global::System.Net.Http.MultipartFormDataContent();
-                            __httpRequestContent.Add(
-                                content: new global::System.Net.Http.StringContent(fileId.ToString() ?? string.Empty),
-                                name: "\"file_id\"");
-
                             var __contentFile = new global::System.Net.Http.StreamContent(file);
                             __contentFile.Headers.ContentType = new global::System.Net.Http.Headers.MediaTypeHeaderValue(
                                 request.Filename is null
